658f9e11c8aa7675cfb803a451da1ac58603a1ac
noheliaeuceda <noheliaeucedaf@gmail.com>
2020-06-14 15:34:53 -0600

    Atributos corregidos

--- Jugador.cpp
+++ Jugador.cpp
@@ -1,14 +1,12 @@
 #include "Jugador.h"
 
-Jugador::Jugador(string nombre, int anioCursado, string casa, int numeroCamiseta, int peso, int reflejo, int musculatura)
+Jugador::Jugador(string nombre, int anioCursado, string casa, int numeroCamiseta)
 {
 	this->nombre=nombre;
 	this->anioCursado = anioCursado;
 	this->casa=casa;
 	this->numeroCamiseta=numeroCamiseta;
-	this->peso=peso;
-	this->reflejo=reflejo;
-	this->musculatura=musculatura;
+	
 }
 
 string Jugador::getNombre(){
@@ -38,27 +36,6 @@ int Jugador::getNumeroCamista(){
 }
 void Jugador::setNumeroCamista(int numeroCamiseta){
 	this->numeroCamiseta=numeroCamiseta;
-}
-int Jugador::getPeso(){
-	return this->peso;
-	
-}
-void Jugador::setPeso(int peso){
-	this->peso=peso;
-}
-int Jugador::getReflejo(){
-	return this->reflejo;
-	
-}
-void Jugador::setReflejo(int reflejo){
-	this->reflejo=reflejo;
-}
-int Jugador::getMusculatura(){
-	return this->musculatura;
-	
-}
-void Jugador::setMusculatura(int musculatura){
-	this->musculatura=musculatura;
 }
 
 
